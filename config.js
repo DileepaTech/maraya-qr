00805c0fc32c19905d48b1803a597a371f475b0c
DarkWinzo <darkwinzo4440@gmail.com>
2023-06-16 02:31:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-global.bot_name = "",
+global.bot_name = "𝗤𝚄𝙴𝙴𝙽-𝗔𝙸",
 global.mongodb_url = "",
 global.session_footer = "queen~",
-global.github_link = "",
-global.yt_channel_link = ""
+global.github_link = "https://github.com/DarkWinzo",
+global.yt_channel_link = "https://youtube.com/@darkwinzo"
