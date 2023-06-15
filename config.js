fa644a5b0909b78b932fcedf87be6fb9e425df64
DarkWinzo <darkwinzo4440@gmail.com>
2023-06-16 01:51:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+global.bot_name = "",
+global.mongodb_url = "",
+global.session_footer = "queen~",
+global.github_link = "",
+global.yt_channel_link = ""
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+const app = express();
+let path = require('path')
+let cookieParser = require('cookie-parser');
+let createError = require('http-errors')
+const cors = require('cors'),
+      secure = require('ssl-express-www'),
+      mongoose = require('mongoose');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+let api = require('./routes/api'),
+    server = require('./routes/server');
+
+app.set('trust proxy', true);
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(cookieParser());
+app.use(express.static("public"))
+app.use('/api', api)
+app.use('/server', server)
+
+
+app.listen(PORT, () => {
+    console.log(`【﻿ＤＡＲＫＷＩＮＺＯ ＱＲ　ＡＰＩ】
+								 
+ Server running on http://localhost:` + PORT)
+console.log(`RUNNING YOUR QR CODE`)
+})
+
+module.exports = app
